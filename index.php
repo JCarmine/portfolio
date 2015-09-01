15b86a767d371267cbb2bdce33c60982de33b7cd
Jesse Carmine <carminej@gmail.com>
2015-09-01 16:40:35 -0400

    Final Tweaks.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <!--[if lt IE 8]>
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
-    <h1>I create unique and elegant websites that both look great and work perfectly, on any device.</h1>
+    <h1 class="visuallyhidden">I create unique and elegant websites that both look great and work perfectly, on any device.</h1>
     <div id="contentSlide">
         <!--Nav-->
         <header>
