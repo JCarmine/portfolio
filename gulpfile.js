f9a39d6bc55f5ca04cd61ccfe748dde6180eef8e
Jesse Carmine <jesse@revup.com>
2019-12-27 19:29:05 -0800

    Cleanup the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -8,71 +8,77 @@ var gulp = require("gulp"),
     browserSync = require("browser-sync").create();
 
 var paths = {
-    styles: {
-        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-        src: "./styles/scss/*.scss",
-        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
-        dest: "./styles/"
-    }
+  styles: {
+    // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+    src: "./styles/scss/*.scss",
+    // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+    dest: "./styles/"
+  }
 
-    // Easily add additional paths
-    // ,html: {
-    //  src: '...',
-    //  dest: '...'
-    // }
+  // Easily add additional paths
+  // ,html: {
+  //  src: '...',
+  //  dest: '...'
+  // }
+};
+
+function phpServer() {
+  php.server({
+    base:'./',
+    port:8000,
+    keepalive:true
+  });
 };
 
 function style() {
-    return gulp
-        .src(paths.styles.src)
-        // Initialize sourcemaps before compilation starts
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .on("error", sass.logError)
-        // Use postcss with autoprefixer and compress the compiled file using cssnano
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // Now add/write the sourcemaps
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.styles.dest))
-        // Add browsersync stream pipe after compilation
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.styles.src)
+    // Initialize sourcemaps before compilation starts
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on("error", sass.logError)
+    // Use postcss with autoprefixer and compress the compiled file using cssnano
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    // Now add/write the sourcemaps
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    // Add browsersync stream pipe after compilation
+    .pipe(browserSync.stream());
 }
-
-
-function phpServer() {
-    php.server({base:'./', port:8000, keepalive:true});
-};
 
 // A simple task to reload the page
 function reload() {
-    browserSync.reload();
+  browserSync.reload();
 }
 
 // Add browsersync initialization at the start of the watch task
 function watch() {
-    browserSync.init({
-        // You can tell browserSync to use this directory and serve it as a mini-server
+  browserSync.init({
+    // If you are already serving your website locally using something like gulp-connect-php
+    // You can use the proxy setting to proxy that instead
+    // proxy: "yourlocal.dev"
+    proxy: "localhost:8000",
+    notify: false
 
-        proxy: "localhost:8000",
-        notify: false
-        // If you are already serving your website locally using something like apache
-        // You can use the proxy setting to proxy that instead
-        // proxy: "yourlocal.dev"
-    });
-    gulp.watch(paths.styles.src, style);
-    // We should tell gulp which files to watch to trigger the reload
-    // This can be html or whatever you're using to develop your website
-    // Note -- you can obviously add the path to the Paths object
-    //gulp.watch("./*.html", reload);
-    gulp.watch("./*.php").on('change', browserSync.reload);
+    // Or, you can tell browserSync to use this directory and serve it as a mini-server
+      server: {
+        baseDir: "./"
+      }
+  });
+  gulp.watch(paths.styles.src, style);
+  // We should tell gulp which files to watch to trigger the reload
+  // This can be html or whatever you're using to develop your website
+  // Note -- you can obviously add the path to the Paths object
+  //gulp.watch("./*.php", reload);
+  gulp.watch("./index.php").on('change', browserSync.reload);
 }
 
 // We don't have to expose the reload function
 // It's currently only useful in other functions
 
+// Don't forget to expose the tasks!
 exports.phpServer = phpServer;
 
-// Don't forget to expose the task!
 exports.watch = watch
 
 // Expose the task by exporting it
